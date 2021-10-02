b03e1038968d943d276f7f77d372cd2fd71717a2
LisandroCanteros <canteros.lisandro.a@gmail.com>
2021-10-02 19:28:16 -0300

    agregado ejercicio 1

---
+++ Complementarios-Level1/Ejercicio1/src/com/informatorio/Main.java
@@ -0,0 +1,15 @@
+package com.informatorio;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String nombre;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Ingrese nombre de usuario");
+        nombre = scan.nextLine();
+        System.out.println("----------------------------");
+        System.out.println("HOLA " + nombre + "!!!");
+    }
+}
